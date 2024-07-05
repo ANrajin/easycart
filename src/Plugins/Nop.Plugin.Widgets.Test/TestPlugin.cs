5f91a8d11fbe577c9ee7b87508b9958da9e97dc5
anrajin <an.rajin@gmail.com>
2024-07-05 14:35:42 +0600

    [update]-test plugin added

---
+++ src/Plugins/Nop.Plugin.Widgets.Test/TestPlugin.cs
@@ -0,0 +1,16 @@
+﻿using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Widgets.Test;
+
+public class TestPlugin:BasePlugin
+{
+    public override async Task InstallAsync()
+    {
+        await base.InstallAsync();
+    }
+
+    public override async Task UninstallAsync()
+    {
+        await base.UninstallAsync();
+    }
+}
