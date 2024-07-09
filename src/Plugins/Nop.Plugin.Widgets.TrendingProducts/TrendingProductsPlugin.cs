96b1d741c33018d484e809d4cc6c7237dbbb59f9
anrajin <an.rajin@gmail.com>
2024-07-09 12:12:27 +0600

    [update]-new widget plugin created

---
+++ src/Plugins/Nop.Plugin.Widgets.TrendingProducts/TrendingProductsPlugin.cs
@@ -0,0 +1,17 @@
+﻿using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Widgets.TrendingProducts
+{
+    public class TrendingProductsPlugin:BasePlugin
+    {
+        public override async Task InstallAsync()
+        {
+            await base.InstallAsync();
+        }
+
+        public override async Task UninstallAsync()
+        {
+            await base.UninstallAsync();
+        }
+    }
+}
