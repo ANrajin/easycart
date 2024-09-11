a8dc1aa74d30aacc51ed290527f78d69526f1e2b
anrajin <an.rajin@gmail.com>
2024-09-11 16:06:53 +0600

    new plugin created

---
+++ src/Plugins/Nop.Plugin.Misc.GoogleShoppingFeed/Controllers/GoogleShoppingFeedController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Controllers;
+
+namespace Nop.Plugin.Misc.GoogleShoppingFeed.Controllers;
+public class GoogleShoppingFeedController : BaseController
+{
+    public async Task<IActionResult> Configure()
+    {
+        return View();
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Misc.GoogleShoppingFeed/GoogleShoppingFeedPlugin.cs
@@ -0,0 +1,24 @@
+﻿using Nop.Core;
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Misc.GoogleShoppingFeed;
+
+public class GoogleShoppingFeedPlugin(IWebHelper webHelper) : BasePlugin
+{
+    private readonly IWebHelper _webHelper = webHelper;
+
+    public override string GetConfigurationPageUrl()
+    {
+        return $"{_webHelper.GetStoreLocation()}Admin/GoogleShoppingFeed/Configure";
+    }
+
+    public override async Task InstallAsync()
+    {
+        await base.InstallAsync();
+    }
+
+    public override async Task UninstallAsync()
+    {
+        await base.UninstallAsync();
+    }
+}
